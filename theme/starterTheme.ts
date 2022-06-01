aaa9c706a163062206557a3958601b74b546d96e
Adrian <a.roszak@josera.pl>
2022-06-01 13:26:54 +0200

    feat(CU-2je5m2t): separate object with custom typography config && change css var prefix

--- theme/starterTheme.ts
+++ theme/starterTheme.ts
@@ -1,6 +1,7 @@
 import { extendTheme } from '@chakra-ui/react'
 import spacing from './partials/spacing'
 import colors from './partials/colors'
+import typography from './partials/typography'
 
 function responsive(
   size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl'
@@ -17,54 +18,7 @@ function responsive(
 
 const starterTheme = extendTheme({
   space: spacing,
-  // Typography
-  fonts: {
-    heading: 'Architects Daughter',
-    body: 'Architects Daughter',
-  },
-  fontSizes: {
-    xs: '7px',
-    sm: '10px',
-    md: 'var(--allec--ts--content-fs)',
-    lg: 'var(--allec--ts--h6-fs)',
-    xl: 'var(--allec--ts--h5-fs)',
-    '2xl': 'var(--allec--ts--h4-fs)',
-    '3xl': 'var(--allec--ts--h3-fs)',
-    '4xl': 'var(--allec--ts--h2-fs)',
-    '5xl': 'var(--allec--ts--h1-fs)',
-  },
-  fontWeights: {
-    light: 300,
-    normal: 400,
-    medium: 500,
-    bold: 700,
-  },
-  lineHeights: {
-    normal: 'var(--allec--ts--content-lh)',
-    none: '1',
-    shorter: '1.25',
-    short: '1.375',
-    base: '1.5',
-    tall: '1.625',
-    taller: '2',
-    xs: '7px',
-    sm: '10px',
-    md: 'var(--allec--ts--content-lh)',
-    lg: 'var(--allec--ts--h6-lh)',
-    xl: 'var(--allec--ts--h5-lh)',
-    '2xl': 'var(--allec--ts--h4-lh)',
-    '3xl': 'var(--allec--ts--h3-lh)',
-    '4xl': 'var(--allec--ts--h2-lh)',
-    '5xl': 'var(--allec--ts--h1-lh)',
-  },
-  letterSpacings: {
-    tighter: '-0.05em',
-    tight: '-0.025em',
-    normal: '0',
-    wide: '0.025em',
-    wider: '0.05em',
-    widest: '0.1em',
-  },
+  ...typography,
   colors,
   components: {
     Heading: {
@@ -164,6 +118,9 @@ const starterTheme = extendTheme({
         colorScheme: 'grey',
       },
     },
+  },
+  config: {
+    cssVarPrefix: 'allec',
   },
 })
 
